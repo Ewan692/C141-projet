3c99277c5872290743e44751e4af252d9d99a2ce
Ewan Pfister <ewan.pfister@divcom.ch>
2026-03-25 11:56:03 +0100

    Feat: pages favoris et à propos + navigation et icônes

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,31 @@
 <template>
   <!-- En-tête du site -->
-  <v-app-bar color="primary" prominent>
-    <v-app-bar-title>
-      <v-icon icon="mdi-alien" class="mr-2" />
-      Games Catalog
-    </v-app-bar-title>
+  <v-app-bar flat>
+    <!-- Icônes -->
+    <v-avatar
+        class="mr-4 pa-0 cursor-pointer"
+        image="@/images/nintendo-game-boy.svg" alt="Icône de Game Boy"
+        size="64"
+        @click="$router.push('/')"
+    />
+
+    <!-- Titre -->
+    <v-toolbar-title>Games Catalog</v-toolbar-title>
+
+    <!-- Menu de navigation -->
+    <v-btn
+        v-for="link in menuItems"
+        :key="link.title"
+        :icon="link.icon"
+        :to="link.path"
+    />
   </v-app-bar>
 </template>
 
-<script setup></script>
+<script setup>
+const menuItems = [
+  { title: 'Accueil', path: '/', icon: 'mdi-nintendo-game-boy' },
+  { title: 'Favoris', path: '/favoris', icon: 'mdi-heart' },
+  { title: 'À propos', path: '/a-propos', icon: 'mdi-information' },
+]
+</script>
---
+++ src/pages/favoris.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container>
+    <!-- Titre de la page -->
+    <h1 class="text-h3 text-center my-6">Mes favoris</h1>
+
+    <!-- Message informatif si aucun jeu favori -->
+    <v-alert type="info" variant="tonal">
+      Vous n'avez pas encore de jeu favori.
+    </v-alert>
+  </v-container>
+</template>
+
+<script setup></script>
