4a1967d47ea235f1888d8ba802c32a061d319a13
Ewan Pfister <ewan.pfister@divcom.ch>
2026-03-25 11:01:19 +0100

    Feat: affichage grille jeux depuis l'API

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,47 +1,62 @@
 <template>
   <v-container>
-    <h1 class="text-h4 my-4">Personnages Rick & Morty</h1>
+    <h1 class="text-h4 my-4">GAMES</h1>
 
-    <!-- Chargement (skeleton) -->
-    <v-row v-if="loading">
-      <v-col v-for="n in 8" :key="n" cols="12" sm="6" md="4" lg="3">
-        <v-skeleton-loader type="image, heading, text" />
-      </v-col>
-    </v-row>
+    <v-row>
+      <v-col
+          v-for="game in games"
+          :key="game.id"
+          cols="12"
+          sm="6"
+          md="4"
+          lg="3"
+      >
+        <v-card>
 
-    <!-- Erreur -->
-    <v-alert v-else-if="error" type="error" class="my-4">
-      {{ error }}
-    </v-alert>
+          <v-img
+              :src="game.background_image"
+              height="200"
+              cover
+          />
 
-    <!-- TODO : Liste des personnages -->
-    <v-row v-else>
-      <v-col cols="12">
-        <p>{{ characters.length }} personnage(s) chargé(s)</p>
+          <v-card-title>
+            {{ game.name }}
+          </v-card-title>
+
+          <v-card-subtitle>
+            Rating {{ game.rating }}
+          </v-card-subtitle>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
+import {onMounted, ref} from "vue";
 
-// État réactif
-const characters = ref([])
-const loading = ref(true)
-const error = ref(null)
+const games = ref([])
 
-// TODO : Charger les personnages depuis l'API Rick and Morty
-// URL : https://rickandmortyapi.com/api/character
-// La réponse contient { info: {...}, results: [...] }
+try {
+// onMounted s'exécute quand le composant est monté
 onMounted(async () => {
-  try {
-    // TODO : fetch + vérification response.ok + extraction des résultats
+  const res = await fetch(
+      `https://api.rawg.io/api/games?key=65431f6a205b4ae0899f6dade712f408`
+  )
 
-  } catch (err) {
-    error.value = `Impossible de charger les personnages : ${err.message}`
-  } finally {
-    loading.value = false
+  // Conversion de la réponse en JSON si elle ne l'est pas déjà
+  const data = await res.json()
+
+  // On stocke uniquement le tableau "results" contenu dans la réponse json
+  // Si le tableau "results" ne contient rien, on met un tableau vide
+  if (data.results) {
+    games.value = data.results;
+  } else {
+    games.value = [];
   }
+  console.log(games.value)
 })
+} catch (error) {
+  console.error('Erreur lors de la récupération des jeux:', error)
+}
 </script>
