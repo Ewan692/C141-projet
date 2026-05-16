e2f01ef970c1cbd29eabb508d564470f6d241cdb
Ewan Pfister <ewan.pfister@divcom.ch>
2026-05-16 15:59:09 +0200

    Amélioration du footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,10 +1,38 @@
 <template>
-  <v-footer>
-    <div class="px-4 text-center w-100">
-      <!-- Slot par défaut si rien n'est affiché -->
-      <slot>C141 - ESIG {{ new Date().getFullYear() }} | Games Catalog | ©Ewan Pfister</slot>
-    </div>
+  <v-footer class="border-t" color="#183b4f">
+    <!-- Slot par défaut si rien n'est affiché -->
+    <slot>
+      <v-container>
+        <div class="d-flex flex-column flex-md-row align-center justify-space-between py-4">
+          <div class="text-body-1 mb-2 mb-md-0">
+            🎮 Games Catalog
+          </div>
+
+          <div class="text-body-1 text-medium-emphasis">
+            C141 - ESIG • {{ new Date().getFullYear() }}
+          </div>
+
+          <div class="text-body-1 mt-2 mt-md-0">
+            © Ewan Pfister
+          </div>
+
+          <!-- Bouton retour en haut -->
+          <v-btn class="mt-2"
+              icon="mdi-arrow-up"
+              variant="tonal"
+              @click="scrollToTop"
+          />
+        </div>
+      </v-container>
+    </slot>
   </v-footer>
 </template>
 
-<script setup></script>
+<script setup>
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  })
+}
+</script>
